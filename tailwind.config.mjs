9041d49ff25df669962229f229223f422f3a5b5f
sion-jaison <sionjaison@gmail.com>
2026-01-21 06:19:38 +0000

    Update to Premium Portfolio

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    darkMode: 'class', // Enables toggle support
 	theme: {
-		extend: {},
+		extend: {
+            fontFamily: {
+                sans: ['Manrope', 'sans-serif'],
+            },
+        },
 	},
 	plugins: [],
 }
